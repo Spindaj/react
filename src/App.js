b4b12f8352b00e549f4b49dd7c39c7c717faa5a8
Spindaj <107429735+Spindaj@users.noreply.github.com>
2023-06-19 12:18:48 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,29 @@
+export default function App(){
+
+  return(
+
+    <div class = 'container'>
+
+      <div class = 'comentario'>
+
+        <Comentario />
+
+      </div>
+
+      <div class = 'comentario'>
+
+        <Comentario />
+
+      </div>
+
+      <div class = 'comentario'>
+
+        <Comentario />
+
+      </div>
+
+    </div>
+
+  )
+
+}
